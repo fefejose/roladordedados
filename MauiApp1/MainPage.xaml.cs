9780a9bf391ce2014050ffbb3c3d1f461479878a
fefejose <fefegomes040108@gmail.com>
2024-09-06 21:58:28 -0300

    Mensagem de atualização

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void botao_Clicked(object sender, EventArgs e)
+        {
+        Random random = new Random();
+            int roladorDeDados = random.Next(PickerNumeros.SelectedItem);
+
+
+            botao.Text = $"Resultado{roladorDeDados}";
+        }
+
+        private void botao_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
